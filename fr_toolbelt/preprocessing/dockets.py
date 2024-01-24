8feb9dcb9499fa00dd5bce602b428652492d24d6
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-01-24 14:40:30 -0500

    Update dockets.py

--- fr_toolbelt/preprocessing/dockets.py
+++ fr_toolbelt/preprocessing/dockets.py
@@ -1,33 +1,41 @@
 # no imports; all native python
 
 
-def extract_docket_info(document: dict, 
-                        key: str = "regulations_dot_gov_info"):
-    
-    docket_info = document.get(key, {})
-    
-    values = []
-    if len(docket_info) == 0:
-        values = None
-    elif isinstance(docket_info, list):
-        for docket in docket_info:
-            values.append(docket.get('docket_id'))
-    elif isinstance(docket_info, dict):
-        values.append(docket_info.get('docket_id'))
+class RegsDotGovData:
 
-    if values is not None:
-        values = "; ".join(v for v in values if v is not None)
-    
-    return values
+    def __init__(self, documents: list[dict], key: str = "regulations_dot_gov_info") -> None:
+        self.documents = documents
+        self.key = key
 
-
-def create_docket_keys(document: dict, 
-                       values: tuple = None):
-    
-    document_copy = document.copy()
+    def extract_docket_info(self, document: dict) -> str | None:
         
-    document_copy.update({
-        "docket_id": values, 
-        })
+        docket_info = document.get(self.key, {})
+        
+        if len(docket_info) == 0:
+            values = None
+        elif isinstance(docket_info, list):
+            values = [docket.get("docket_id") for docket in docket_info]
+        elif isinstance(docket_info, dict):
+            values = [docket_info.get("docket_id")]
+
+        if values is not None:
+            values = "; ".join(v for v in values if v is not None)
+        
+        return values
+
+    def create_docket_key(document: dict, values: str = None) -> dict:
+        
+        document_copy = document.copy()
+            
+        document_copy.update({
+            "docket_id": values, 
+            })
+        
+        return document_copy
     
-    return document_copy
+    def process_docket_data(self) -> list[dict]:
+        return [self.create_docket_key(doc, values=self.extract_docket_info(doc)) for doc in self.documents]
+
+if __name__ == "__main__":
+    
+    pass
