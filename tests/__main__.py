9f83fe63bd3f51ab5edfb35d4f382ae47eaec77f
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-02-01 13:31:15 -0500

    Update __main__.py
    add profiling to tests module

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,8 +1,16 @@
+import time
 from .tests import ALL_TESTS
 
 if __name__ == "__main__":
+
+    # profile time elapsed
+    start = time.process_time()
     
+    # run test suite
     for func in ALL_TESTS:
         func()
-    
     print("Tests complete.")
+
+    # calculate time elapsed
+    stop = time.process_time()
+    print(f"CPU time: {stop - start:0.1f} seconds")
