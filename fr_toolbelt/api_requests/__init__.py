fa06fa7ffdc47029235eefa4afdceda87ad61bcf
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-02-08 17:59:32 -0500

    Update __init__.py

--- fr_toolbelt/api_requests/__init__.py
+++ fr_toolbelt/api_requests/__init__.py
@@ -3,14 +3,18 @@ Making requests from the Federal Register API.
 """
 
 __all__ = [
-    "duplicates", 
-    "format_dates", 
-    "get_documents", 
+    "identify_duplicates", 
+    "remove_duplicates",
+    "DateFormatter", 
+    "DateFormatError", 
+    "get_documents_by_date", 
+    "get_documents_by_number", 
+    "QueryError",
     ]
 
 
-from .duplicates import *
+from .duplicates import identify_duplicates, remove_duplicates
 
 from .format_dates import DateFormatter, DateFormatError
 
-from .get_documents import *
+from .get_documents import get_documents_by_date, get_documents_by_number, QueryError
