32dffbdd99ea7f9d91aa60c5e21c7e713536f52d
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-02-08 20:32:55 -0500

    Update __init__.py

--- fr_toolbelt/__init__.py
+++ fr_toolbelt/__init__.py
@@ -1,8 +1,6 @@
-from fr_toolbelt import api_requests
-from fr_toolbelt import preprocessing
-
-__title__ = "fr-toolbelt"
-__version__ = "0.1.0"
+"""
+fr-toolbelt
+"""
 
 __all__ = [
     "api_requests", 
@@ -10,3 +8,9 @@ __all__ = [
     "__title__", 
     "__version__",
 ]
+
+__title__ = "fr-toolbelt"
+__version__ = "0.1.0"
+
+from fr_toolbelt import api_requests
+from fr_toolbelt import preprocessing
