64bdb5a7fd8d4dea3eee45817b9e6432191a4ce5
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-02-13 13:24:02 -0500

    Update __main__.py
    convert main to take input

--- fr_toolbelt/__main__.py
+++ fr_toolbelt/__main__.py
@@ -1,6 +1,4 @@
-if __name__ == "__main__":
-    print("Running this package as a script from the command line provides a quick way to test its functionality.")
-    
+def main():
     from pprint import pprint
 
     from fr_toolbelt.api_requests import (
@@ -18,52 +16,56 @@ if __name__ == "__main__":
     
     start = "2024-01-01"
     end = "2024-01-31"
-    fields = [
-        "document_number", 
-        "publication_date", 
-        "agency_names", 
-        "agencies", 
-        "title", 
-        "type", 
-        "regulation_id_number_info", 
-        #"regulations_dot_gov_info", 
-        "dockets", 
-        "docket_ids", 
-        "president", 
-        ]
-    results, count = get_documents_by_date(start, end, fields=fields)
-    print("\n\n-- RAW --\n")
+    results, count = get_documents_by_date(start, end)
+    print(f"Retrieved {count} documents published from {start} to {end}.")
+    print("\n\n-- PRINTING AN ILLUSTRATIVE DOCUMENT BEFORE PROCESSING --\n")
     pprint(results[0])
     
     metadata, schema = AgencyMetadata().get_agency_metadata()
     agency_data = AgencyData(results, metadata, schema)
     processed_agencies = agency_data.process_data(return_format="name")
-    print("\n\n-- AGENCIES --\n")
+    print("\n\n-- AFTER PROCESSING AGENCIES --\n")
     pprint(processed_agencies[0])
     
     dockets = Dockets(processed_agencies)
     processed_dockets = dockets.process_data(del_keys="docket_ids")
-    print("\n\n-- DOCKETS --\n")
+    print("\n\n-- AFTER PROCESSING DOCKETS (REGULATIONS.GOV INFO) --\n")
     pprint(processed_dockets[0])
     
     presidents = Presidents(processed_dockets)
     processed_presidents = presidents.process_data()
-    print("\n\n-- PRESIDENTS --\n")
+    print("\n\n-- AFTER PROCESSING PRESIDENTS --\n")
     pprint(processed_presidents[0])
     
     rin = RegInfoData(processed_presidents)
     processed_rin = rin.process_data()
-    print("\n\n-- RIN --\n")
+    print("\n\n-- AFTER PROCESSING RIN (REGINFO.GOV INFO) --\n")
     pprint(processed_rin[0])
     
     processed_docs = process_documents(results)
-    print("\n\n-- ALL --\n")
+    print("\n\n-- AFTER PROCESSING ALL FIELDS --\n")
     pprint(processed_docs[0])
 
-    processed_docs = process_documents(results, del_keys="type")
-    print("\n\n-- ALL (del 1 key) --\n")
+    del_keys = "type"
+    processed_docs = process_documents(results, del_keys=del_keys)
+    print(f"\n\n-- AFTER PROCESSING ALL FIELDS AND DELETING 1 FIELD ({del_keys}) --\n")
     pprint(processed_docs[0])
 
-    processed_docs = process_documents(results, del_keys=("type", "docket_ids"), return_values_as_str=False, identify_ira=False)
-    print("\n\n-- ALL (del 2 keys) --\n")
+    del_keys = ("type", "docket_ids")
+    processed_docs = process_documents(results, del_keys=del_keys, return_values_as_str=False, identify_ira=False)
+    print(f"\n\n-- AFTER PROCESSING ALL FIELDS, DELETING 2 FIELDS ({"; ".join(del_keys)}), RETURNING AGENCY INFO AS LISTS, AND NOT IDENTIFYING INDEPENDENT REG AGENCIES --\n")
     pprint(processed_docs[0])
+
+
+if __name__ == "__main__":
+    print("Running this package as a script from the command line provides a quick way to test its functionality.")
+    proceed = input("Would you like to proceed? [yes/no] >>> ").lower()
+    while True:
+        if proceed in ("yes", "y"):
+            main()
+            break
+        elif proceed in ("no", "n"):
+            break
+        else:
+            proceed = input("Invalid input.\nWould you like to proceed? [yes/no] >>> ").lower()
+            continue
