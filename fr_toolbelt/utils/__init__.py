f9be4b493699d205c58e5e19f6114aae88de0228
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-02-16 16:23:03 -0500

    Update __init__.py

--- fr_toolbelt/utils/__init__.py
+++ fr_toolbelt/utils/__init__.py
@@ -2,13 +2,20 @@
 Utilities for other fr-toolbelt modules and beyond.
 """
 
-from .duplicates import identify_duplicates, remove_duplicates, process_duplicates, DuplicateError
+from .duplicates import (
+    identify_duplicates, 
+    remove_duplicates, 
+    flag_duplicates, 
+    process_duplicates, 
+    DuplicateError
+    )
 
 from .format_dates import DateFormatter, DateFormatError
 
 __all__ = [
     "identify_duplicates", 
     "remove_duplicates",
+    "flag_duplicates",
     "process_duplicates",
     "DuplicateError",
     "DateFormatter", 
