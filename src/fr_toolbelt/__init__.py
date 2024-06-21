5026a3f56fbbfe4fda64899143ef493bcca8f525
mfebrizio <mfebrizio@gwu.edu>
2024-06-20 20:34:38 -0400

    Update __init__.py

--- src/fr_toolbelt/__init__.py
+++ src/fr_toolbelt/__init__.py
@@ -4,12 +4,12 @@ fr-toolbelt
 Toolbelt of classes and functions written in Python to use with the Federal Register (FR) API.
 """
 
-from fr_toolbelt import api_requests
-from fr_toolbelt import preprocessing
-from fr_toolbelt import utils
+from .fr_toolbelt import api_requests
+from .fr_toolbelt import preprocessing
+from .fr_toolbelt import utils
 
 __title__ = "fr-toolbelt"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     "api_requests", 
